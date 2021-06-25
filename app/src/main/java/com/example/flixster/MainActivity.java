54947d06a385d270f31db23ea3d11fc96aeaa513
Julius Snipes <jsnipes@fb.com>
2021-06-24 20:54:22 -0400

    Added spinner that allows user to choose what type of movies they want to see

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -27,10 +33,12 @@ import okhttp3.Headers;
 public class MainActivity extends AppCompatActivity {
 
     // URL with JSON data on movies playing now from TMDB
-    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=bd1591c106e1d2d6b0f1c487f82bd791";
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/%s?api_key=bd1591c106e1d2d6b0f1c487f82bd791";
     public static final String TAG = "MainActivity";
 
     List<Movie> movies;
+    String movieOption;
+    MovieAdapter movieAdapter;
 
 
     @Override
@@ -42,16 +50,49 @@ public class MainActivity extends AppCompatActivity {
         movies = new ArrayList<>();
 
         // Create the adapter
-        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+        movieAdapter = new MovieAdapter(this, movies);
         // Set the adapter on the recycler view
         binding.rvMovies.setAdapter(movieAdapter);
         // Set a Layout Manager on the recycler view
         binding.rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
-        AsyncHttpClient client = new AsyncHttpClient();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.movie_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                movies.clear();
+                movieOption = parent.getItemAtPosition(position).toString();
+                AsyncHttpClient client = new AsyncHttpClient();
+                updateList(movieOption, client, movieAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+
+            }
+        });
+        return true;
+    }
+
+    private void updateList(String option, AsyncHttpClient client, MovieAdapter movieAdapter) {
         // Get the JSON data from the TMDB url
-        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+        client.get(String.format(NOW_PLAYING_URL, option.toLowerCase().replaceAll(" ", "_")), new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int i, Headers headers, JSON json) {
                 Log.d(TAG,"onSuccess");
